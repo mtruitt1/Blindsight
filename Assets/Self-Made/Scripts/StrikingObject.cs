02efc9edbad53eb2413c34a14065bc78193ff06b
MTruitt <mark.truitt.0@gmail.com>
2020-11-21 18:33:30 -0500

    Changed physics materials to prevent rocks bouncing all over, made rocks freeze after 10 seconds of inactivity and despawn if they fall below y = -100

--- Assets/Self-Made/Scripts/StrikingObject.cs
+++ Assets/Self-Made/Scripts/StrikingObject.cs
@@ -7,10 +7,35 @@ public class StrikingObject : SoundObject {
     public float strengthMult = 1f;
     public float minColMag = 1f;
     public float maxColMag = 1000f;
+    public float freezeTimer = -1f;
+    private float freezeClock;
+    private Rigidbody rb;
+
+    protected override void Start() {
+        base.Start();
+        rb = GetComponent<Rigidbody>();
+        freezeClock = freezeTimer;
+    }
 
     private void OnCollisionEnter(Collision collision) {
         float strength = Mathf.Clamp(collision.relativeVelocity.magnitude, minColMag, maxColMag) * strengthMult;
         PlayRandSound(strength, true, false);
         //Debug.Log("Sound made, strength: " + strength + " @ " + transform.position.ToString());
+    }
+
+    private void Update() {
+        if (freezeClock > 0f) {
+            if (rb.velocity.sqrMagnitude <= 0.001f) {
+                freezeClock -= Time.deltaTime;
+                if (freezeClock <= 0f) {
+                    Destroy(rb);
+                }
+            } else {
+                freezeClock = freezeTimer;
+            }
+        }
+        if (transform.position.y <= -100f) {
+            Destroy(gameObject);
+        }
     }
 }
