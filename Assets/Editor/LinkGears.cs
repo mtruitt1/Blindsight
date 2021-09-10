2e88ea4cba805b815424d095b0b0c3e4c1554919
M Truitt <53928003+mtruitt1@users.noreply.github.com>
2021-09-10 01:07:34 -0400

    Added an extra level, fixed some issues with scripts, made pressure plates and elevators

---
+++ Assets/Editor/LinkGears.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class GearLinker : EditorWindow {
+    [MenuItem("Window/Link gears in level")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(GearLinker));
+    }
+
+    void OnGUI() {
+        if (GUILayout.Button("Link")) {
+            List<Gear> allGears = new List<Gear>();
+            Dictionary<int, Gear> orderedGears = new Dictionary<int, Gear>();
+            allGears.AddRange(FindObjectsOfType<Gear>());
+            foreach (Gear gear in allGears) {
+                int number = (gear.name.Contains("Counter") ? 1 : 0) + (2 * (gear.name.Contains("(") ? int.Parse(Regex.Match(gear.name, @"\d+").Value) : 0));
+                orderedGears.Add(number, gear);
+            }
+            for (int i = 0; i < orderedGears.Count; i++) {
+                if (orderedGears[i].bouncables.Count == 0 && i != 0) {
+                    orderedGears[i].bouncables.Add(orderedGears[i - 1]);
+                }
+                //Debug.Log("Gear \"" + orderedGears[i].name + "\" is " + i + "th in the list");
+            }
+        }
+    }
+}
